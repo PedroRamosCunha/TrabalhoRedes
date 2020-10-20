840811d71d3dd7f995c4eae2dc8ee7fc0eec55e3
Gabriela Vidal <ga28bvidal@gmail.com>
2020-10-20 19:02:10 -0300

    Código comentado

--- socket_cliente.c
+++ socket_cliente.c
@@ -1,35 +1,45 @@
+/*
+Arqiuvo Sonda (Cliente).
+O que faz:
+-Coleta dados ambientais
+-Envia os dados para a base (servidor)
+*/
+
+//Bibliotecas utilizadas
 #include <time.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-#include <arpa/inet.h>    //permite que use in_port_t e in_add_t do netinet
-
-/*
--Sonda (Cliente)
--Coleta dados ambientais
--Envia para a base (servidor)
-*/
-
+#include <arpa/inet.h>
 
 //Funcoes que geram dados falsos
-float sensor_press(){
-	return (730.0+((float)(rand()%50)/20)); //mmHg
-}
-float sensor_temp(){
-	return (0.0+((float)(50-rand()%100)/20)); // Celsius
-}
-float sensor_hum(){
-	return (33.3+((float)(50-rand()%100)/40)); // Humidade Relativa %
+float sensor_press(){	//Função que "coleta" os dados de pressao (em mmHh) que a sonda esta recebendo
+	return (730.0+((float)(rand()%50)/20));
 }
 
-float gps_lat(float last){
+float sensor_temp(){	//Função que "coleta" os dados de temperatura (em °C) que a sonda esta recebendo
+	return (0.0+((float)(50-rand()%100)/20));
+}
+
+float sensor_hum(){	//Função que "coleta" os dados de humidade relativa (em %) que a sonda esta recebendo
+	return (33.3+((float)(50-rand()%100)/40));
+}
+
+float gps_lat(float last){	//Função que "coleta" os dados de latitude do GPS instalado na sonda
+	last+=((float)(10-rand()%20)/100.0);
+
+	if(fmod((double)last, 1.0)>=0.6){
+		last+=0.4;
+	}
+	return last;
+}
+
+float gps_long(float last){	//Função que "coleta" os dados de longitude do GPS instalado na sonda
 	last+=((float)(10-rand()%20)/100.0);
 
 	if(fmod((double)last, 1.0)>=0.6){
@@ -38,23 +48,12 @@ float gps_lat(float last){
 
 	return last;
 }
-float gps_long(float last){
-	last+=((float)(10-rand()%20)/100.0);
-
-	if(fmod((double)last, 1.0)>=0.6){
-		last+=0.4;
-	}
-
-	return last;
-}
-
 
 int main(){
-	long int porta;
-	int ans_connect;
+	long int porta;	//porta de conexao com o servidor
+	int ans_connect;	//resposta de conexao com o servidor
 	int socket_servidor;
 	struct sockaddr_in addr;
-
 	float last_lat, last_long;
 	int recebidos, enviados;
 	char mensagem[64];
@@ -67,42 +66,41 @@ int main(){
 	//Seed do aleatorio
 	srand(time(0)); 
 
-	socket_servidor = socket(AF_INET,SOCK_STREAM,0);
+	socket_servidor = socket(AF_INET,SOCK_STREAM,0);	//cria um socket de fluxo com protocolo TCP
 
-	if(socket_servidor == -1){
+	if(socket_servidor == -1){	//Condicao para caso a criacao do socket tenha falhado
 		printf("Falha ao criar socket\n");
 		return -1;
 	}
 
-	//Funções do <netinet.h>
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(1234);   //Porta de conexao
+	addr.sin_port = htons(1234);   //Porta de conexao inicial
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	//fim
 
 	memset(&(addr.sin_zero),0,sizeof(addr.sin_zero));
 
+//Apos criar o socket, o cliente espera para receber o comunicado de que foi conectado ao servidor
 	printf("Tentando se conectar a base...\n");
 
-	if(connect(socket_servidor,(struct sockaddr*)&addr,sizeof(addr)) == -1)  {
+	if(connect(socket_servidor,(struct sockaddr*)&addr,sizeof(addr)) == -1){	//Condicao para caso a conexao com o servidor tenha falhado por algum motivo
 		printf("Erro em connect()\n");
 		return -2;
 	}
 	printf("Conectado\n\n");
 
 	//Recebe porta da base
-	recebidos = recv(socket_servidor,resposta,64,0);
+	recebidos = recv(socket_servidor,resposta,64,0);	//Recebe o numero da porta que ocorrera o envio dos dados da sonda
 	resposta[recebidos] = '\0';
 
-	porta = strtol(resposta, NULL, 10);
+	porta = strtol(resposta, NULL, 10);		//Converte a string de resposta em um long int
 	printf("Porta: %ld\n", porta);
 
-	for(int i=0; i<500000000; i++){
+	for(int i=0; i<500000000; i++){				//2 for para que o sistema não rode muito rapidamente e seja possível visualizar o que esta acontecendo
 		for(int j; j<500000000; j++){}
 	}
 
 	close(socket_servidor);         
-	socket_servidor = socket(AF_INET,SOCK_STREAM,0);
+	socket_servidor = socket(AF_INET,SOCK_STREAM,0);	//Cria um novo protocolo para abrigar a nova interacao entre cliente e servidor com a porta que eles trocarão informações
 
 	if(socket_servidor == -1){
 		printf("Falha ao criar socket\n");
@@ -110,18 +108,18 @@ int main(){
 	}
 
 
+	//Inicializacao da nova conexao com o servidor pela porta enviada por ele para transmissao dos dados
 	//Conecta na porta fornecida pela base
-	//Funções do <netinet.h>
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(1234+porta);   //Porta de conexao
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	//fim
 
 	memset(&(addr.sin_zero),0,sizeof(addr.sin_zero));
 
+	//Apos criar o socket, o cliente espera para receber o comunicado de que foi conectado ao servidor
 	printf("Tentando se conectar a base...\n");
 
-	if(connect(socket_servidor,(struct sockaddr*)&addr,sizeof(addr)) == -1)  {
+	if(connect(socket_servidor,(struct sockaddr*)&addr,sizeof(addr)) == -1){	//Condicao para caso a conexao com o servidor tenha falhado por algum motivo
 		printf("Erro em connect()\n");
 		return -2;
 	}
@@ -142,7 +140,7 @@ int main(){
 		recebidos = recv(socket_servidor,resposta,64,0);
 		resposta[recebidos] = '\0';
 		printf("Base: %s\n",resposta);
-	}while(enviados != -1); // Enquanto nao ha retorno de erro
+	}while(enviados != -1); // Enquanto nao ha erro
 
 
 	close(socket_servidor);         
--- socket_servidor.c
+++ socket_servidor.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h> 
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/socket.h> 
-
-#include <arpa/inet.h>    //permite que use in_port_t e in_add_t do netinet
-#include <netinet/in.h>
-
-#include <pthread.h>
-
 /*
-Ground Station (servidor):
+Arqiuvo Ground Station (servidor).
+O que faz:
 - recebe os dados (do cliente)
 - exibe na tela
 */
 
+//Bibliotecas utilizadas
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+//Funcao para analisar as mensagens recebidas das sondas
 void parse_dados(float *dados, char *mensagem){
 	char buffer[16];
-
 	int i, pos1, pos2;
 
 	pos1 = 0;
-	for(i=0; i<5; i++){
+	for(i=0; i<5; i++){		//Loop para separar a mensagem recebida dentro de uma matriz que receberá os dados separadamente para ser mostrado no terminal
 		pos2=0;
 		while(pos1 < 64 && mensagem[pos1]!=';' && pos2 < 15){
 			buffer[pos2] = mensagem[pos1];
@@ -32,50 +31,48 @@ void parse_dados(float *dados, char *mensagem){
 		}
 		pos1++;
 		buffer[pos2]='\0';
-		dados[i] = strtof(buffer,NULL);
+		dados[i] = strtof(buffer,NULL);		//Transferência da informação já separada presente no buffer para a variável dados, que será printada na main
 	}
 }
 
-
-
+//Funcao que conecta a sonda ao ground station
 void *conectar_sonda(void *arg){
 	long int porta = *(long int *)arg;
-	int    meu_socket;
-	int    sock_cliente;
+	int meu_socket;
+	int sock_cliente;
 	struct sockaddr_in addr;
 
 	long int count = 0;
 	int recebidos, enviados; // Controle de bytes enviados e recebidos
 	char mensagem[64];      // Buffer para envio de mensagens
 	char resposta[64];     // Buffer para receber mensagens
-
 	float dados[5];
 
-	meu_socket = socket(AF_INET,SOCK_STREAM,0);
+	meu_socket = socket(AF_INET,SOCK_STREAM,0);		//cria um socket de fluxo com protocolo TCP
 
-	if(meu_socket == -1){
+	if(meu_socket == -1){		//Condicao para caso a criacao do socket tenha falhado
 		printf("Falha ao criar Socket\n");
 		pthread_exit(NULL);
 	}
 
 	addr.sin_family      = AF_INET;
-	addr.sin_port        = htons(1234+porta);
+	addr.sin_port        = htons(1234+porta);	//Cria uma porta que e igual a porta inicial de conexao+numero da nova porta para transmissao de dados
 	addr.sin_addr.s_addr = INADDR_ANY;
 	memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
 
-	if(bind(meu_socket,(struct sockaddr*)&addr,sizeof(addr)) == -1){
+	if(bind(meu_socket,(struct sockaddr*)&addr,sizeof(addr)) == -1){	//Associa a porta criada ao sistema e analisa se ela ja nao esta sendo utilizada. Caso esteja, retorna um erro
 		printf("Erro em bind()\n");
 		pthread_exit(NULL);
 	}
 
-	if(listen(meu_socket,1) == -1){
+	if(listen(meu_socket,1) == -1){	//Habilita que o servidor se conecte com o cliente
 		printf("Erro em listen()\n");
 		pthread_exit(NULL);
 	}
 
 	printf("Aguardando cliente...\n");
 
-	sock_cliente = accept(meu_socket,0,0);
+	sock_cliente = accept(meu_socket,0,0);	//Funcao que aceita as conexoes com os clientes
 
 	if(sock_cliente == -1){
 		printf("Erro em accept()\n");
@@ -92,6 +89,7 @@ void *conectar_sonda(void *arg){
 		//Faz o parse dos dados e os exibe
 		parse_dados(dados, resposta);
 
+		//Printa os dados recebidos
 		printf("\nSonda %ld\n", porta);
 		printf("Coordenadas: (%.2f,%.2f)\n",dados[0],dados[1]);
 		printf("Pressao: %.2f\n",dados[2]);
@@ -99,44 +97,43 @@ void *conectar_sonda(void *arg){
 		printf("Humidade: %.2f\n",dados[4]);
 
 
-		for(int i=0; i<400000000; i++){
+		for(int i=0; i<400000000; i++){	//2 for para que o sistema nao rode muito rapidamente e seja possivel visualizar o que está acontecendo
 			for(int j; j<400000000; j++){}
 		}
 
-		//Enviado a confirmação para a sonda
+		//Envia a confirmação para a sonda
 		count++;
 		sprintf(mensagem, "%ld-OK", count);
 		enviados = send(sock_cliente,mensagem,strlen(mensagem),0);
 
-	}while(enviados != -1 ); // Enquanto nao ha retorno de erro
+	}while(enviados != -1 ); // Enquanto nao ha erro
 	
+	//Apos acabar de receber as informações, o socket e a thread sao fechados
 	close(sock_cliente);
 	close(meu_socket);
 	pthread_exit(NULL);
 }
 
-
 int main(){ 
 	int    meu_socket;
 	int    sock_cliente;
 	struct sockaddr_in addr;
-
 	long int count = 0;
 	char mensagem[64];      // Buffer para envio de mensagens
 	int enviados;          // Controle de bytes enviados
 
-	pthread_t thread;
+	pthread_t thread;		//Criacao de thread para conseguir receber dados simultaneos de varias sondas ao redor do mundo
 	pthread_attr_t attr;
 
-	meu_socket = socket(AF_INET,SOCK_STREAM,0);
+	meu_socket = socket(AF_INET,SOCK_STREAM,0);	//cria um socket de fluxo com protocolo TCP
 
-	if(meu_socket == -1){
+	if(meu_socket == -1){		//Condicao para caso a criacao do socket tenha falhado
 		printf("Falha ao criar Socket\n");
 		return -1;
 	}
 
 	addr.sin_family      = AF_INET;
-	addr.sin_port        = htons(1234);
+	addr.sin_port        = htons(1234);	//Porta de conexao inicial com o cliente, para entao envia-lo para outra porta e receber os dados
 	addr.sin_addr.s_addr = INADDR_ANY;
 	memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
 
@@ -150,7 +147,7 @@ int main(){
 		return -3;
 	}
 
-	do{
+	do{		//While para aguardar cliente chamar o servidor e criar a conexao
 		printf("Aguardando cliente...\n");
 
 		sock_cliente = accept(meu_socket,0,0);
@@ -164,17 +161,17 @@ int main(){
 
 		printf("Sonda %ld chegou\n", count);
 
-		//Enviado a confirmação para a sonda
+		//Envia a confirmação para a sonda
 		sprintf(mensagem, "%ld", count);
 		enviados = send(sock_cliente,mensagem,strlen(mensagem),0);
 
 		//Cria a thread de servidor para a sonda
 		pthread_attr_init(&attr);
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-		pthread_create(&thread, &attr, conectar_sonda, &count);
+		pthread_create(&thread, &attr, conectar_sonda, &count);		//a thread e iniciada e entra na funcao que a sonda transmite os dados para o servidor
 
 		close(sock_cliente);
-	}while(enviados != -1 ); // Enquanto nao ha retorno de erro
+	}while(enviados != -1 ); // Enquanto nao ha erro
 
 	close(sock_cliente);
 	close(meu_socket);
